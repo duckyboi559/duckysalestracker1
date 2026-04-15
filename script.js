a8508bb520965b2e6017d7202e31041db3bf2924
duckyboi559 <adriancuenguzman2024@gmail.com>
2026-04-14 22:41:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  databaseURL: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyDMhniulSEmiIWoXzwJy6zVSOZkHELhfLc", 
+ authDomain: "family-sales.firebaseapp.com", 
+ databaseURL: "https://family-sales-default-rtdb.firebaseio.com", projectId: "family-sales", 
+ storageBucket: "family-sales.firebasestorage.app", 
+ messagingSenderId: "590845027956",
+ appId: "1:590845027956:web:676df074fe6150e8d39321"
+};
 };
 
 const app = initializeApp(firebaseConfig);
